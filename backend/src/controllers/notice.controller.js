d70e908db2a46f5518d62083110db400b2c17705
Madhav Jivani <202201285@daiict.ac.in>
2024-12-21 21:43:05 +0530

    Add CRUD operations for Notices: implement get, update, and delete functionalities with appropriate error handling

--- backend/src/controllers/notice.controller.js
+++ backend/src/controllers/notice.controller.js
@@ -50,3 +50,116 @@ export const createNotice = async (req, res) => {
             .json({ error: "Internal Server Error", success: false });
     }
 };
+
+export const getNotices = async (req, res) => { 
+    try {
+        const notices = await Notice.find().sort({createdAt: -1});
+        if(!notices) {
+            return res
+                .status(404)
+                .json({ error: "No Notices found", success: false });
+        }
+        return res
+            .status(200)
+            .json({ message: "Notices found successfully", success: true , notices: notices });
+    } catch (error) {
+        console.log(`Error in getNotices: ${error.message}`);
+        return res
+            .status(500)
+            .json({ error: "Internal Server Error", success: false });
+    }
+};
+
+export const getNotice = async (req, res) => { 
+    try {
+        const notice = await Notice.findById(req.params.id);
+        if(!notice) {
+            return res
+                .status(404)
+                .json({ error: "Notice not found", success: false });
+        }
+        return res
+            .status(200)
+            .json({ message: "Notice found successfully", success: true , notice: notice });
+    } catch (error) {
+        console.log(`Error in getNotice: ${error.message}`);
+        return res
+            .status(500)
+            .json({ error: "Internal Server Error", success: false });
+    }
+};
+
+export const updateNotice = async (req, res) => { 
+    try {
+        const notice = await Notice.findById(req.params.id);
+        if(!notice) {
+            return res
+                .status(404)
+                .json({ error: "Notice not found", success: false });
+        }
+        const { title, short_description, detailed_description } = req.body;
+        if (!title || !short_description || !detailed_description) { 
+            return res
+                .status(400)
+                .json({ error: "Please enter all the fields", success: false });
+        }
+        try {
+            notice.title = title;
+            notice.short_description = short_description;
+            notice.detailed_description = detailed_description;
+            const updatedNotice = await notice.save();
+            if(!updatedNotice) {
+                return res
+                    .status(500)
+                    .json({ error: "Error while updating the Notice", success: false });
+            }
+            return res
+                .status(201)
+                .json({ message: "Notice updated successfully", success: true , notice: updatedNotice });
+        } catch (error) {
+            console.log(`Error while updating the Notice: ${error.message}`);
+            return res
+                .status(500)
+                .json({ error: "Error while updating the Notice", success: false });
+            
+        }
+    } catch (error) {
+        console.log(`Error in updateNotice: ${error.message}`);
+        return res
+            .status(500)
+            .json({ error: "Internal Server Error", success: false });
+        
+    }
+};
+
+export const deleteNotice = async (req, res) => { 
+    try {
+        const notice = await Notice.findById(req.params.id);
+        if(!notice) {
+            return res
+                .status(404)
+                .json({ error: "Notice not found", success: false });
+        }
+        try {
+            const deletedNotice = await Notice.deleteOne({ _id: notice._id });
+            if(!deletedNotice) {
+            return res
+                .status(500)
+                .json({ error: "Error while deleting the Notice", success: false });
+            }
+        } catch (error) {
+            console.log(`Error while deleting the Notice: ${error.message}`);
+            return res
+            .status(500)
+            .json({ error: "Error while deleting the Notice", success: false });
+        }
+        return res
+            .status(200)
+            .json({ message: "Notice deleted successfully", success: true });
+    } catch (error) {
+        console.log(`Error in deleteNotice: ${error.message}`);
+        return res
+            .status(500)
+            .json({ error: "Internal Server Error", success: false });
+    }
+};
--- backend/src/routes/notice.routes.js
+++ backend/src/routes/notice.routes.js
@@ -1,10 +1,15 @@
 import  { Router } from "express";
-import { createNotice } from '../controllers/notice.controller.js';
-import { verifyJWT, verifyTeacherOrAdmin } from '../middlewares/auth.middleware.js'
+import { createNotice,getNotices,getNotice,updateNotice,deleteNotice } from '../controllers/notice.controller.js';
+import { verifyJWT, verifyTeacherOrAdmin, verifyAdmin } from '../middlewares/auth.middleware.js'
 import { upload } from '../middlewares/multer.middleware.js';
+
 
 const router = Router();
 
-router.route('/create-notice').post(upload.none(),verifyJWT, verifyTeacherOrAdmin, createNotice);
+router.route('/create-notice').post(upload.none(), verifyJWT, verifyTeacherOrAdmin, createNotice);
+router.route('/get-notices').get(upload.none(), verifyJWT, getNotices);
+router.route('/get-notice/:id').get(upload.none(), verifyJWT, getNotice);
+router.route('/update-notice/:id').put(upload.none(), verifyJWT, verifyAdmin, updateNotice);
+router.route('/delete-notice/:id').delete(upload.none(), verifyJWT, verifyAdmin, deleteNotice);
 
 export default router;
